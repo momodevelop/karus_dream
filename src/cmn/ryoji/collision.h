0d2f16d092f0d0b2a5bda893bbad8f3acfbd5459
Gerald Wong <ramza88@gmail.com>
2019-08-23 22:25:10 +0800

    Update collision.h

--- src/cmn/ryoji/collision.h
+++ src/cmn/ryoji/collision.h
@@ -3,6 +3,17 @@
 
 // simple code for detecting collision in 2D
 namespace collision {
+	template<typename T>
+	bool isLinesColliding(T lhsLineMin, T lhsLineMax, T rhsLineMin, T rhsLineMax) {
+		if (lhsLineMax >= rhsLineMin) return true;
+		if (lhsLineMin <= rhsLineMax) return true;
+		return false;
+	}
+
+	template<typename T>
+	T getCollidingLinesPushout(T lhsLineMin, T lhsLineMax, T rhsLineMin, T rhsLineMax) {
+
+	}
 
 }
 
